82aa8b32421197b5a72bf5faf6edf2dd91fa272f
SzymonKuhn <szymon.kuhn@gmail.com>
2019-02-23 12:21:41 +0100

    init

---
+++ src/ZmiennePrzyklady.java
@@ -0,0 +1,27 @@
+public class ZmiennePrzyklady {
+    /**
+     * komentarz do całej klasy
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!!!");
+        System.out.println("Witaj!");
+        System.out.println("Szymon");
+        System.out.println("Szymon");
+        System.out.println("Szymon");
+
+        int liczbaCalkowita = 13456;
+        double liczbaZmiennoprzecinkowa = 134.55;
+        char uniCode;
+        int symbolChar = 64178;
+        uniCode = symbolChar;
+
+        System.out.println(liczbaCalkowita);
+        System.out.println(uniCode);
+        System.out.println(liczbaZmiennoprzecinkowa);
+        System.out.println(symbolChar);
+
+    }
+}
